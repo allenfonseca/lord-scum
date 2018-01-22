171d4191bf4bb6e24d4ff092bf544cb1a895cdd3
Allen Fonseca <allenfonseca@gmail.com>
2018-01-21 21:43:38 -0500

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,5 @@
 
-//lazy load ===================================
+// LAZY LOAD ===================================
 // https://webdesign.tutsplus.com/tutorials/how-to-lazy-load-embedded-youtube-videos--cms-26743
 
 var youtube = document.querySelectorAll( ".youtube" );
@@ -28,4 +28,42 @@ for (var i = 0; i < youtube.length; i++) {
     });
 }
 
-//end lazy load ================================
+// END LAZY LOAD ================================
+
+// READ MORE ===================================
+// https://github.com/justmarkup/demos/blob/gh-pages/toggle-content/showmore/with-js/index.html
+
+if ('querySelector' in document && 
+    'addEventListener' in window) {
+    var toggleButtons = document.querySelectorAll('.toggle-content');
+    var fullTextWrappers = document.querySelectorAll('.fulltext');
+    var fullText;
+    var toggleButtonText;
+    
+    [].forEach.call(fullTextWrappers, function(fullTextWrapper) {
+        // hide all full text on load
+        fullTextWrapper.setAttribute('hidden', true);
+    });
+    [].forEach.call(toggleButtons, function(toggleButton) {
+        // show toggle more buttons
+        toggleButton.removeAttribute('hidden');
+        // add listener for each button
+        toggleButton.addEventListener('click', function () {
+            fullTextWrapper = this.parentElement.querySelector('.fulltext');
+            toggleButtonText = this.querySelector('.more');
+            // change attributes and text if full text is shown/hidden
+            console.log(fullTextWrapper.hasAttribute('hidden'));
+            if (!fullTextWrapper.hasAttribute('hidden')) {
+                toggleButtonText.innerText = 'Unroll The Scroll...';
+                fullTextWrapper.setAttribute('hidden', true);
+                toggleButton.setAttribute('aria-expanded', false);
+            } else {
+                toggleButtonText.innerText = 'Roll The Scroll...';
+                fullTextWrapper.removeAttribute('hidden');
+                toggleButton.setAttribute('aria-expanded', true);
+            }
+        });
+    });
+}
+
+// END READ MORE ================================
